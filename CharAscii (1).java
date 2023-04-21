1d8184cd2ca5cb3c004107015618fd02a2175b91
UtshaMishra <utshamishra99@gmail.com>
2023-04-21 10:20:40 +0530

    done

---
+++ CharAscii (1).java
@@ -0,0 +1,18 @@
+package day4_nestedLoop_Strings;
+
+public class CharAscii {
+
+	public static void main(String[] args) {
+		 
+	    
+		for(int a=97;a<=122;a++)
+			System.out.print(" "+(char)a);
+		
+		System.out.println();
+		
+		for(int b=65;b<=90;b++)
+			System.out.print(" "+(char)b);
+
+	}
+
+}
